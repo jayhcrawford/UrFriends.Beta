2fccb039cb3303b3ed9703973790013b82f111d9
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-02-06 01:03:38 -0800

    100_Obliterate initial calendar attempt

--- UrFriends/src/components/Calendar.jsx
+++ UrFriends/src/components/Calendar.jsx
@@ -3,6 +3,7 @@ import LinkBar from "./LinkBar";
 import useWindowSize from "../functions/WindowResize";
 
 
+
 const Calendar = () => {
   return (
     <div className="calendar-base" style={{backgroundColor: "red", height: "45em", width: "100"}}>
