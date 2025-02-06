51a8076d1c0c0a3166167d4d1d61cd15ce2d6e1d
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-02-06 01:30:02 -0800

    103_Adds programmatic day rendering

--- UrFriends/src/components/Calendar.jsx
+++ UrFriends/src/components/Calendar.jsx
@@ -2,15 +2,62 @@ import React, { useState } from "react";
 import LinkBar from "./LinkBar";
 import useWindowSize from "../functions/WindowResize";
 
-const days = Array(42).fill(null);
+const days = [
+  "a1",
+  "a2",
+  "a3",
+  "a4",
+  "a5",
+  "a6",
+  "a7",
+  "b1",
+  "b2",
+  "b3",
+  "b4",
+  "b5",
+  "b6",
+  "b7",
+  "c1",
+  "c2",
+  "c3",
+  "c4",
+  "c5",
+  "c6",
+  "c7",
+  "d1",
+  "d2",
+  "d3",
+  "d4",
+  "d5",
+  "d6",
+  "d7",
+  "e1",
+  "e2",
+  "e3",
+  "e4",
+  "e5",
+  "e6",
+  "e7",
+  "f1",
+  "f2",
+  "f3",
+  "f4",
+  "f5",
+  "f6",
+  "f7",
+];
 
 const RenderCalendarDays = () => {
   return (
     <>
-    {}
+      {days.map((day) => {
+        return (
+          <div style={{border: "1px solid black"}} key={day}>{day}</div>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
 const date = new Date();
 const monthIndex = date.getMonth(); // Returns a number from 0 (January) to 11 (December)
@@ -134,7 +181,7 @@ const Calendar = () => {
             gridRowGap: "0px",
           }}
         >
-
+          <RenderCalendarDays />
         </div>
       </div>
     </>
