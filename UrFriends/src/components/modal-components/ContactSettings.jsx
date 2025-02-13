1d7b115328c01de3da9903d5999395523e77c3b4
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-02-12 22:57:35 -0800

    108_Refactors(WIP) ContactSettings.jsx modal

--- UrFriends/src/components/modal-components/ContactSettings.jsx
+++ UrFriends/src/components/modal-components/ContactSettings.jsx
@@ -8,10 +8,48 @@ import {
 } from "../../features/notificationSlice";
 import { populatePhonebook } from "../../features/phonebookSlice";
 
+//static; renders delete and prompts message
+const DeleteThisContact = (props) => {
+  return (
+    <>
+      {!props.areYouSure && (
+        <button onClick={props.handleAreYouSure}>delete this contact</button>
+      )}
+      {props.areYouSure && (
+        <div>
+          <div style={{ backgroundColor: "red", color: "white" }}>
+            Everything related to this contact (including conversations) will be
+            deleted, and it is irreversible. Are you sure?
+          </div>
+          <button
+            style={{
+              margin: "1em",
+              border: "3px solid red",
+              cursor: "pointer",
+            }}
+            onClick={props.handleDelete}
+          >
+            Yes, Delete Everything
+          </button>
+          <br />
+          <button
+            style={{ margin: "1em" }}
+            onClick={() => props.setAreYouSure(false)}
+          >
+            Nevermind
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+//export
 const ContactSettings = () => {
   const person = useSelector((state) => state.modal.person);
   const phonebookStore = useSelector((state) => state.phonebook.phonebook);
 
+  //for delete contact
   const [areYouSure, setAreYouSure] = useState(false);
 
   const dispatch = useDispatch();
@@ -60,34 +98,23 @@ const ContactSettings = () => {
   return (
     <div>
       <h2>Settings for {person.name.first}</h2>
+
+      {/* Entire form disappears if the delete button is clicked, and a warning is shown */}
       {!areYouSure && (
-        <button onClick={handleAreYouSure}>delete this contact</button>
-      )}
-      {areYouSure && (
         <div>
-          <div style={{ backgroundColor: "red", color: "white" }}>
-            Everything related to this contact (including conversations) will be
-            deleted, and it is irreversible. Are you sure?
+          <div>
+            
           </div>
-          <button
-            style={{
-              margin: "1em",
-              border: "3px solid red",
-              cursor: "pointer",
-            }}
-            onClick={handleDelete}
-          >
-            Yes, Delete Everything
-          </button>
-          <br />
-          <button
-            style={{ margin: "1em" }}
-            onClick={() => setAreYouSure(false)}
-          >
-            Nevermind
-          </button>
         </div>
       )}
+
+      {/* Contains delete button and warning message */}
+      <DeleteThisContact
+        handleAreYouSure={handleAreYouSure}
+        setAreYouSure={setAreYouSure}
+        areYouSure={areYouSure}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
