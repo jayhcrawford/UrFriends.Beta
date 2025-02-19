d9dc3c54d31f8a1a0565b90f901e80c8587f2d4e
Jay Crawford <jayhcrawford@gmail.com>
2025-02-19 02:46:01 -0800

    111_ Adds ChangeableInput component to ContactSettings.jsx

--- UrFriends/src/components/modal-components/ContactSettings.jsx
+++ UrFriends/src/components/modal-components/ContactSettings.jsx
@@ -9,6 +9,110 @@ import {
 import { populatePhonebook } from "../../features/phonebookSlice";
 import { sendNotification } from "../../functions/sendNotification";
 
+const ModifyDataButton = (props) => {
+  return (
+    <button onClick={() => props.edit(props.keyToEdit)}>{props.text}</button>
+  );
+};
+
+//an input element that modifies state stored higher in the form
+const ChangeableInput = (props) => {
+  return (
+    <>
+      {props.editBool && (
+        <span>
+          {props.label}{" "}
+          <input
+            value={props.valueProp ? props.valueProp : null}
+            onChange={(event) =>
+              props.changePerson(props.personValue, event.target.value)
+            }
+          ></input>
+          <ModifyDataButton
+            text="Save"
+            edit={props.setEditMode}
+            keyToEdit={props.personValue}
+          />
+        </span>
+      )}
+      {!props.editBool && (
+        <span>
+          {props.label} {props.valueProp}
+          <ModifyDataButton
+            text="Edit"
+            edit={props.setEditMode}
+            keyToEdit={props.personValue}
+          />
+        </span>
+      )}
+    </>
+  );
+};
+
+const ContactSettingsForm = () => {
+  const personStore = useSelector((state) => state.modal.person);
+  
+  const [person, setPerson] = useState({
+    firstName: personStore.name.first,
+    lastName: "",
+  });
+  const [editPerson, setEditPerson] = useState({
+    firstName: false,
+    lastName: false,
+  });
+
+  //changes whether or not a field is being edited. If a field in editPerson is true, edit is enabled.
+  const setEditMode = (keyToChange) => {
+    const newEditPersonObj = {
+      ...editPerson,
+      [keyToChange]: !editPerson[keyToChange],
+    };
+
+    setEditPerson(newEditPersonObj);
+  };
+
+  //changes the object that stores the actual Contact values
+  const changePerson = (keyToChange, change) => {
+    const newPerson = {
+      ...person,
+      [keyToChange]: change,
+    };
+    setPerson(newPerson);
+  };
+
+  const gridStyle = {
+    display: "grid",
+    gridTemplateRows: "repeat(6, 1fr)", // 6 rows
+    gridTemplateColumns: "repeat(2, 1fr)", // 2 columns
+    gap: "10px", // optional gap between items
+  };
+
+  return (
+    <div style={gridStyle}>
+      <div>
+        <ChangeableInput
+          label={"First Name"}
+          valueProp={person.firstName}
+          editBool={editPerson.firstName}
+          personValue={"firstName"}
+          editPerson={editPerson}
+          person={person}
+          changePerson={changePerson}
+          setEditMode={setEditMode}
+        />
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      
+    </div>
+  );
+};
+
 //static; renders delete and prompts message
 const DeleteThisContact = (props) => {
   return (
@@ -98,6 +202,8 @@ const ContactSettings = () => {
   return (
     <div>
       <h2>Settings for {person.name.first}</h2>
+
+      <ContactSettingsForm />
 
       {/* Entire form disappears if the delete button is clicked, and a warning is shown */}
       {!areYouSure && (
