415aba1a10184583446f79f824158242cedff24d
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-06-19 00:21:20 -0400

    fully authCallback is back

--- UrFriends/src/components/AuthCallback.jsx
+++ UrFriends/src/components/AuthCallback.jsx
@@ -15,25 +15,20 @@ const AuthCallback = () => {
     navigate("/");
 
 
-
-    console.log("code at home is", code, state)
-
-    // const urlParams = new URLSearchParams(window.location.search);
-    // const code = urlParams.get("code");
-    // if (code) {
-    //   fetch("https://td236amhd1.execute-api.us-east-2.amazonaws.com/main/api", {
-    //     method: "POST",
-    //     credentials: "omit",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({ code }),
-    //   })
-    //     .then((res) => {
-    //       console.log(res, "is the auth response");
-    //       if (res.ok) navigate("/main");
-    //       else throw new Error("Login failed");
-    //     })
-    //     .catch((err) => console.error(err));
-    // }
+    if (code) {
+      fetch("https://td236amhd1.execute-api.us-east-2.amazonaws.com/main/api", {
+        method: "POST",
+        credentials: "omit",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code, state }),
+      })
+        .then((res) => {
+          console.log(res, "is the auth response");
+          if (res.ok) navigate("/main");
+          else throw new Error("Login failed");
+        })
+        .catch((err) => console.error(err));
+    }
   }, [searchParams]);
 
   return <div>Processing login...</div>;
