62621c2697982b4e65a402e0fb39d7c2d14521c4
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-06-19 00:13:28 -0400

    call navigate in authCallback

--- UrFriends/src/components/AuthCallback.jsx
+++ UrFriends/src/components/AuthCallback.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 //Catches the auth info from cognito and posts it to the backend
 
 const AuthCallback = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
   const state = searchParams.get("state");
@@ -12,6 +12,7 @@ const AuthCallback = () => {
   useEffect(() => {
     console.log(code, "is code");
     console.log(state, "is state");
+    navigate("/")
   }, [searchParams]);
 
   return <div>Processing login...</div>;
