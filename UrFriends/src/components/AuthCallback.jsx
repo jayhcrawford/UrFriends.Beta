36e32e290e5e1cbc188169364c291568d4c05a94
jayhcrawford <141302428+jayhcrawford@users.noreply.github.com>
2025-06-19 00:18:36 -0400

    pulls back old authCallback

--- UrFriends/src/components/AuthCallback.jsx
+++ UrFriends/src/components/AuthCallback.jsx
@@ -12,7 +12,28 @@ const AuthCallback = () => {
   useEffect(() => {
     console.log(code, "is code");
     console.log(state, "is state");
-    navigate("/")
+    navigate("/");
+
+
+
+    console.log("code at home is", code, state)
+
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const code = urlParams.get("code");
+    // if (code) {
+    //   fetch("https://td236amhd1.execute-api.us-east-2.amazonaws.com/main/api", {
+    //     method: "POST",
+    //     credentials: "omit",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({ code }),
+    //   })
+    //     .then((res) => {
+    //       console.log(res, "is the auth response");
+    //       if (res.ok) navigate("/main");
+    //       else throw new Error("Login failed");
+    //     })
+    //     .catch((err) => console.error(err));
+    // }
   }, [searchParams]);
 
   return <div>Processing login...</div>;
